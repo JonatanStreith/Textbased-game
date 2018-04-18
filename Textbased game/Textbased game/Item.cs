969e9bb3f81b42e2c71b5051fb64036fd9f79520
JonatanStreith <jonatan@streith.se>
2018-04-18 13:02:10 +0200

    The basics

---
+++ Textbased game/Textbased game/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Textbased_game
+{
+    class Item
+    {
+
+        public Item() //Constructor
+        {
+                
+        }
+
+    }
+}
---
+++ Textbased game/Textbased game/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Textbased_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //create character
+
+
+            //create locations, creatures, objects and items
+
+
+            while (true)
+            {
+
+                //Continously running play loop that parses instructions
+
+
+            }
+
+
+
+
+
+        }
+    }
+}
